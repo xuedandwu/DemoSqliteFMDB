0f8773e3878c7bb522e17264402c13a02d57716d
Xuedan <xuedan.wu@aiba.com>
2017-03-21 15:31:26 +0800

    修改

---
+++ Desktop/DemoSqliteFMDB/DemoSqlite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoSqlite
+//
+//  Created by zhengtaixiang on 16/10/8.
+//  Copyright © 2016年 zhengtaixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Desktop/DemoSqliteFMDB/DemoSqlite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoSqlite
+//
+//  Created by zhengtaixiang on 16/10/8.
+//  Copyright © 2016年 zhengtaixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
